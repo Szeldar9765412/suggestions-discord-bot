443acfff2b0f31a0591057911825f4f7a757f14b
Neptun <151036094+Szeldar9765412@users.noreply.github.com>
2024-01-11 06:58:05 +0100

    Update Keep_alive.js

--- Keep_alive.js
+++ Keep_alive.js
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
-import requests
-import threading
+from flask import Flask, render_template
+import threading import Thread
 
 app = Flask(__name__)
 
@@ -12,5 +11,5 @@ def run():
     app.run(host='0.0.0.0',port=8080)
 
 def Keep_alive():
-     t= thread(traget=run)
+     t= Thread(traget=run)
      t.start()
